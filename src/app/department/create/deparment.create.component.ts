1ea3b90be73f1b52666e2ffa85e329364f0470e0
Manar-sayed <manarsayed20702070@gmail.com>
2023-06-24 17:11:18 +0200

    department create

--- src/app/department/create/deparment.create.component.ts
+++ src/app/department/create/deparment.create.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 import { Department } from '../../models/Department/department';
 import { DepartmentService } from '../../services/department.service';
 import { ErrorStateMatcher } from '@angular/material/core';
-import { FormBuilder, FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { EmployeeService } from 'src/app/services/employee.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Employee } from 'src/app/models/Employee/employee';
 
 
 export class MyErrorStateMatcher implements ErrorStateMatcher {
@@ -19,61 +21,103 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   templateUrl: './deparment.create.component.html',
   styleUrls: ['./deparment.create.component.scss']
 })
-export class CreateComponent{
-  ngOnInit(){
-    this.empService.getAllEmployees().subscribe((data)=>{
-         this.emps=data;
-         console.log(this.emps)
-    })
-  }
-  check:any=1;
-  emps:any;
+export class CreateComponent {
+  depts!: Department[]; 
+  emps!:Employee[]; 
+  flag:number=0; 
+  flagArray=[0,1];
+  validationMessages = {
+    departmentName: {
+      required: 'You must enter the name of the department',
+      pattern: 'Name should only contain letters',
+    },
+    managerId: {
+      required: 'You must enter the managerId',
+      pattern: 'should only contain numbers',
+    },
+
+
+  };
   constructor(
-    public deptService: DepartmentService,
-    public empService: EmployeeService,
-     public fb: FormBuilder, public router:Router) {}
-  matcher = new MyErrorStateMatcher();
-  createDepartmentForm = this.fb.group({
-    departmentName: [
+    private builder: FormBuilder,
+    private snackBar: MatSnackBar,
+    private deptService: DepartmentService,public fb:FormBuilder,private empService:EmployeeService,public router:Router) {} 
+  ngOnInit() { 
+    this.empService.getAllEmployees().subscribe((emps:any)=>{ 
+      this.emps = emps; 
+      console.log(emps); 
+      
+    }); 
+    this.deptService.getAll().subscribe((depts:any)=>{ 
+      this.depts = depts; 
+      console.log(depts); 
+    }); 
+
+
+  } 
+   
+  createDeptForm = this.builder.group({ 
+    departmentName: this.builder.control(
       '',
-      [
+      Validators.compose([
         Validators.required,
-        Validators.minLength(3),
-        Validators.pattern('[a-zA-Z]*'),
-      ],
-    ],
-    managerId:    ['', [Validators.required]],
-    employessIds: [[], [Validators.required]],
-  
+        Validators.pattern('^[a-zA-Z]+$'),
+      ])
+    ),
+    managerId:  this.builder.control(
+      0,
+      Validators.compose([Validators.required])
+    ),
+    employessIds: this.builder.array([]),
+  }) 
 
-  });
-  getdepartmentName() {
-    return this.createDepartmentForm.get('departmentName');
-  }
-  getmanagerId() {
-    return this.createDepartmentForm.get('managerId');
-  }
-  getemployessIds() {
-    return this.createDepartmentForm.get('employessIds');
-  }
-  createDepartment(): void {
-    this.deptService.createDepartment(this.createDepartmentForm.value,this.check).subscribe(
-      
-      (response) => {this.router.navigateByUrl('/department/list');console.log(this.check); console.log(response)},
-      
-      (error) => {console.log(error);console.log(this.check); console.log(this.getemployessIds()?.value)}
-    );
+  get employeesInProjectIds(): FormArray {
+    return this.createDeptForm.get('employessIds') as FormArray;
   }
 
 
 
 
+  onEmployeeSelectionChange(event: any) {
+    const selectedEmployeeIds = event.value;
+    this.employeesInProjectIds.clear();
+    selectedEmployeeIds.forEach((emplyeeId: string) => {
+      this.employeesInProjectIds.push(this.builder.control(emplyeeId));
+    });
+  }
 
+  onflagChange(event: any) {
+    this.flag= event.value;
 
+  }
 
+  createNewDept(){
+    console.log(this.createDeptForm.value)
+    if (this.createDeptForm.valid) {
+      this.deptService.createDepartment(this.createDeptForm.value,this.flag).subscribe(
+        (response) => {
+          console.log(response);
+          this.snackBar.open('dept added successfully.', 'Close', {
+            duration: 3000,
+          });
+          this.router.navigate(['project']);
+        },
+        (error) => {
+          console.log(error);
+          this.snackBar.open(error.message, 'Close', {
+            duration: 3000,
+          });
+        }
+      );
+    } else {
+      this.snackBar.open('Please enter valid data.', 'Close', {
+        duration: 3000,
+      });
+    }
 
+  }
+  }
 
-}
 
 
 
